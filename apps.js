b13c4fcd840713d6f4fe52b714230a7db32408d3
sam <subterranean.layer@gmail.com>
2022-11-06 12:16:19 -0500

    added board class w/ methods for generating board and initializing pieces

--- apps.js
+++ apps.js
@@ -1,53 +1,63 @@
-/* 
-	Function generates html elements representing the game board.
-		- 	8 square objects populate each of 8 rank objects
-		-	Algebraic notation is notated where necessary
-*/
-function renderBoard() {
+class Board {
 
-	let fileLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-	let color1, color2;
-	let boardArea = document.getElementById('board-area');
 
-	let board = document.getElementById('board');
+	pieces = [];
+	fileLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-	for (let i = 0; i < 8; i++) {
+	constructor() {
+		this.#renderBoard();
+		this.#initializePieces();
+	}
 
-		if ( i % 2 == 0) {
-			color1 = 'dark-square';
-			color2 = 'light-square';
-		}
-		else {
-			color1 = 'light-square';
-			color2 = 'dark-square';
-		}
 
-		for(let j = 0; j < 8; j += 2) {
-			
-			let square = document.createElement('div');
-			square.className = color1;
-			square.classList.add('square');
-			square.id = `${fileLetter[i]}${j + 1}`
-			square.style.gridArea = square.id
-			board.appendChild(square);
+	/* 
+		Method generates html elements representing the game board.
+			- 	8 square objects populate each of 8 rank objects
+			-	Algebraic notation is notated where necessary
+	*/
+	#renderBoard() {
+		let color1, color2;
+		let boardArea = document.getElementById('board-area');
+	
+		let board = document.getElementById('board');
+	
+		for (let i = 0; i < 8; i++) {
+	
+			if ( i % 2 == 0) {
+				color1 = 'dark-square';
+				color2 = 'light-square';
+			}
+			else {
+				color1 = 'light-square';
+				color2 = 'dark-square';
+			}
+	
+			for(let j = 0; j < 8; j += 2) {
+				
+				let square = document.createElement('div');
+				square.className = color1;
+				square.classList.add('square');
+				square.id = `${this.fileLetter[i]}${j + 1}`
+				square.style.gridArea = square.id
+				board.appendChild(square);
+	
+				square = document.createElement('div');
+				square.className = color2;
+				square.classList.add('square');
+				square.id = `${this.fileLetter[i]}${j + 2}`
+				square.style.gridArea = square.id
+				board.appendChild(square);
+			}
+		}	
+		this.notateBoard();
+	}
 
-			square = document.createElement('div');
-			square.className = color2;
-			square.classList.add('square');
-			square.id = `${fileLetter[i]}${j + 2}`
-			square.style.gridArea = square.id
-			board.appendChild(square);
-		}
-	}	
-	notateBoard();
-	console.log(document.getElementsByClassName('piece'));
-}
-/*
+	/*
 	Function notates square with the given file lettering or rank number
 		- 	notation: string -> lettering or numbering to be notated
 		- 	type: string -> either file or rank styling, used for positioning
  */
-function notateBoard () {
+notateBoard () {
 	let fileLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 	let boardContainer = document.getElementById('board-container')
 	for (let i = 0; i < 8; i++) {
@@ -67,44 +77,50 @@ function notateBoard () {
 	}
 }
 
-function initializePieces() {
-	let fileLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-	for(let i = 0; i < 8; i++) {
-		addPiece(`${fileLetter[i]}2`, 'images/piece-icons/white-pawn.png');
-		addPiece(`${fileLetter[i]}7`, 'images/piece-icons/black-pawn.png');
+	#initializePieces() {
+		let pieceType = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+
+		for (let i = 0; i < 16; i++) {
+
+			if(i < 8) {
+				this.pieces.push(new Piece('pawn', 'white', `${this.fileLetter[i % 8]}2`));
+				this.pieces.push(new Piece('pawn', 'black', `${this.fileLetter[i % 8]}7`));
+			}
+			else {
+				this.pieces.push(new Piece(pieceType[i % 8], 'white', `${this.fileLetter[i % 8]}1` ));
+				this.pieces.push(new Piece(pieceType[i % 8], 'black', `${this.fileLetter[i % 8]}8` ));
+			}
+		}
 	}
-	addPiece(`${fileLetter[0]}1`, 'images/piece-icons/white-rook.png');
-	addPiece(`${fileLetter[0]}8`, 'images/piece-icons/black-rook.png');
-	addPiece(`${fileLetter[1]}1`, 'images/piece-icons/white-knight.png');
-	addPiece(`${fileLetter[1]}8`, 'images/piece-icons/black-knight.png');
-	addPiece(`${fileLetter[2]}1`, 'images/piece-icons/white-bishop.png');
-	addPiece(`${fileLetter[2]}8`, 'images/piece-icons/black-bishop.png');
-	addPiece(`${fileLetter[3]}1`, 'images/piece-icons/white-queen.png');
-	addPiece(`${fileLetter[3]}8`, 'images/piece-icons/black-queen.png');
-	addPiece(`${fileLetter[4]}1`, 'images/piece-icons/white-king.png');
-	addPiece(`${fileLetter[4]}8`, 'images/piece-icons/black-king.png');
-	addPiece(`${fileLetter[5]}1`, 'images/piece-icons/white-bishop.png');
-	addPiece(`${fileLetter[5]}8`, 'images/piece-icons/black-bishop.png');
-	addPiece(`${fileLetter[6]}1`, 'images/piece-icons/white-knight.png');
-	addPiece(`${fileLetter[6]}8`, 'images/piece-icons/black-knight.png');
-	addPiece(`${fileLetter[7]}1`, 'images/piece-icons/white-rook.png');
-	addPiece(`${fileLetter[7]}8`, 'images/piece-icons/black-rook.png');
-	
 }
 
-function addPiece(location, url) {
-	let targetSquare = document.getElementById(location);
-	let newPiece = document.createElement('div');
-	newPiece.className = 'piece';
-	newPiece.style.backgroundImage = `url(${url})`;
-	targetSquare.appendChild(newPiece);
+class Piece {
+
+	type;
+	color;
+	location;
+	imageURL;
+
+	constructor(type, color, location) {
+		this.type = type;
+		this.color = color;
+		this.location = location;
+		this.#generatePiece(this.type, this.color, this.location);
+	}
+
+	move(newLocation) {
+		document.getElementById(`${this.type}-${this.location}`).remove();
+		this.location = newLocation;
+		this.#generatePiece(this.type, this.color, this.location);
+	}
+
+	#generatePiece(type, color, location) {
+		let newPiece = document.createElement('div');
+		newPiece.id = `${type}-${location}`;
+		newPiece.className = 'piece';
+		newPiece.style.backgroundImage = `url(images/piece-icons/${color}-${type}.png)`;
+		document.getElementById(this.location).appendChild(newPiece);
+	}
 }
 
-
-function Piece(url, location) {
-	this.url = url;
-	this.location = location;
-}
-
-document.body.addEventListener('onload', renderBoard());
-initializePieces();
+let board = new Board();
